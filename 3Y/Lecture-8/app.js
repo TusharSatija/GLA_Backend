906d2be2c0930201a78d15c0d9fb9d35b8afef4d
TusharSatija <tusharsatija737@gmail.com>
2025-01-29 23:13:39 +0530

    updated changes

--- 3Y/Lecture-8/app.js
+++ 3Y/Lecture-8/app.js
@@ -1,71 +1,29 @@
 let express=require('express');
 let app=express();
-let methodoverride=require('method-override');
-let path= require('path');
+let path=require('path');
 app.set('view engine','ejs');
-app.set(path.join(__dirname,'views'));
-app.use(methodoverride('_method'));
+app.set(path.join(__dirname,"views"));
 
 let blogs=[
     {
         id:1,
-        title:"first blog",
-        imgUrl:"https://images.unsplash.com/photo-1734784547207-7ad9f04c1f0a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        desc:"art graphics paper confetti"
+        title:'first blog',
+        url:"https://images.unsplash.com/photo-1736880705369-e345ec5fa278?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1NXx8fGVufDB8fHx8fA%3D%3D",
+        desc:"animal,wildlife,horse"
     },
     {
         id:2,
-        title:"second blog",
-        imgUrl:"https://images.unsplash.com/photo-1667270532914-788966ce19c3?q=80&w=1927&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        desc:"art modern art paper confettigraphics babypersonHDWallpapers"
+        title:'second blog',
+        url:"https://images.unsplash.com/photo-1736297150541-89378f055b96?q=80&w=1772&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        desc:"jakarta,indonesia,experimental,cityscape"
     }
 ]
-
-//to display all blogs
 app.get('/blogs',(req,res)=>{
     res.render('index',{blogs});
 })
 
-// to create a new blog
 app.get('/blog/new',(req,res)=>{
     res.render('new');
-})
-
-// Show a single blog
-app.get('/blogs/:blogId', (req, res) => {
-    const { blogId } = req.params; 
-    const blog = blogs.find((blog) => blog.id === blogId);
-    if (!blog) {
-        throw new Error('Blog with this id is not present');
-    }
-    res.render('show', { blog });
-});
-
-// Show edit form
-app.get('/blogs/:blogId/edit', (req, res) => {
-    const { blogId } = req.params; 
-    const blog = blogs.find((blog) => blog.id === blogId);
-    if (!blog) {
-        throw new Error('Blog with this id is not present');
-    }
-    res.render('edit', { blog });
-});
-
-// Apply partial modification
-app.patch('/blogs/:blogId', (req, res) => {
-    const { blogId } = req.params;
-    const { title, desc, imgUrl } = req.body;
-    const newBlogs = blogs.map((blog) => blog.id === blogId ? { ...blog, title: title, desc: desc, imgUrl: imgUrl } : blog);
-    blogs = newBlogs;
-    res.redirect(`/blogs/${blogId}`);
-});
-
-// Delete blog
-app.delete('/blogs/:blogId', (req, res) => {
-    const { blogId } = req.params;
-    const newBlogs = blogs.filter((blog) => blog.id !== blogId);
-    blogs = newBlogs;
-    res.redirect('/blogs');
 })
 
 app.listen(3333,()=>{
