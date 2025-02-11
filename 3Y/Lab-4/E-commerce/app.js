139029b10cd8e7dc8afa2a271769413ee6b60c70
TusharSatija <tusharsatija737@gmail.com>
2025-02-11 11:02:23 +0530

    updated E-com

--- 3Y/Lab-4/E-commerce/app.js
+++ 3Y/Lab-4/E-commerce/app.js
@@ -6,6 +6,7 @@ let Product=require('./model/product');
 let path=require('path');
 app.set('view engine','ejs');
 app.set(path.join(__dirname,'views'));
+app.use(express.urlencoded({extended:true}));
 
 async function connect()
 {
@@ -15,9 +16,31 @@ async function connect()
 connect();
 
 
+
+app.get('product/new',(req,res)=>{
+    // res.render('new');
+    res.send('this is a new product route');
+})
+
+app.post('/new',async(req,res)=>{
+    let {title,desc,img_url,price}=req.body;
+    await new Product({title:title,
+        desc:desc,
+        img_url:img_url,
+        price:price
+    })
+    
+})
+
+
 app.get('/product',async (req,res)=>{
     let product1= await  Product.find({});
     res.render('index',{product1});
+})
+
+
+app.get('product/new',(req,res)=>{
+    res.render('new');
 })
 
 async function insert()
