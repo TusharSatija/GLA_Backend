0c0e876670435daaaa70b1a6826127de68fbbfc6
TusharSatija <tusharsatija737@gmail.com>
2025-02-12 10:53:42 +0530

    edit

--- 3AD/Lab-4/e-com/app.js
+++ 3AD/Lab-4/e-com/app.js
@@ -3,11 +3,12 @@ const {  mongoose } = require('mongoose');
 let Product=require('./model/Product');
 let products=require('./seed');
 let app=express();
+let method_override=require('method-override');
 let path=require('path');
 app.set('view engine','ejs');
 app.set(path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
-
+app.use(method_override('_method'));
 async function connect()
 {
    await mongoose.connect('mongodb://127.0.0.1:27017/E-com-3AD');
@@ -35,6 +36,13 @@ app.post('/new',async (req,res)=>{
         img_url:img_url
     });
     res.redirect('product')
+})
+
+app.get('/product/:id/edit',async (req,res)=>{
+    let {_id}=req.params;
+    let p1=await Product.find({_id:_id});
+    console.log(p1);
+    res.render('edit',{p1});
 })
 
 async function insert()
